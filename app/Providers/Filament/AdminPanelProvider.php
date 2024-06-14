bcedf1ca3a34154de9203dd27c655696418f040b
siraj yesuf <sirajyesuf762@gmail.com>
2024-06-15 01:27:45 +0300

    =update the serv prov to the new login hander class

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Pages\Login;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -26,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(Login::class)
             ->colors([
                 // 'primary' => Color::Amber,
                 'primary' => Color::Blue,
