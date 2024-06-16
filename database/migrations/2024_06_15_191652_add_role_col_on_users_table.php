c246f2a73792ab39d326c753cd09f9d350391e86
siraj yesuf <sirajyesuf762@gmail.com>
2024-06-16 23:08:18 +0300

    =role staff fixed

--- database/migrations/2024_06_15_191652_add_role_col_on_users_table.php
+++ database/migrations/2024_06_15_191652_add_role_col_on_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role',Role::toArray());
+            $table->string('role');
         });
     }
 
