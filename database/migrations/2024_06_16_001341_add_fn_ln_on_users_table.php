ccf560109777b14d4d5ae071a63a568909ba4f32
siraj yesuf <sirajyesuf762@gmail.com>
2024-06-16 04:36:04 +0300

    =mig fixed

--- database/migrations/2024_06_16_001341_add_fn_ln_on_users_table.php
+++ database/migrations/2024_06_16_001341_add_fn_ln_on_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->dropColumn('name');
+            $table->dropColumn('name');
             $table->string('first_name');
             $table->string('last_name');
         });
@@ -21,7 +21,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('first_name');
             $table->dropColumn('last_name');
-            // $table->addColumn('string','name',['default' => 'name']);
+            $table->string('name');
         });
     }
 };
