5365faf4525d4bf7dbf32546590eb81b86cd9ff8
siraj yesuf <sirajyesuf762@gmail.com>
2024-06-19 14:50:01 +0300

    =internation call and sms imp and expo done

--- app/Filament/Clusters/Settings/Resources/InternationalRateResource/Pages/ListInternationalRates.php
+++ app/Filament/Clusters/Settings/Resources/InternationalRateResource/Pages/ListInternationalRates.php
@@ -3,13 +3,14 @@
 namespace App\Filament\Clusters\Settings\Resources\InternationalRateResource\Pages;
 
 use App\Filament\Clusters\Settings\Resources\InternationalRateResource;
+use App\Filament\Exports\InternationalRateExporter;
 use App\Models\InternationalRate;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Components\Tab;
 use App\Filament\Imports\VoiceInternationalRateImporter;
+use App\Filament\Imports\TextInternationalRateImporter;
 use Filament\Support\Enums\ActionSize;
-
 
 class ListInternationalRates extends ListRecords
 {
@@ -29,6 +30,28 @@ class ListInternationalRates extends ListRecords
                     'type' => InternationalRate::TYPE_VOICE
                 ]),
 
+                Actions\ImportAction::make()
+                ->label('Import International Text Rate')
+                ->importer(TextInternationalRateImporter::class)
+                ->options([
+                    'updateExisting' => true,
+                    'type' => InternationalRate::TYPE_SMS
+                ])
+            ])
+            ->label('Import')
+            ->icon('heroicon-m-ellipsis-vertical')
+            ->size(ActionSize::Small)
+            ->color('primary')
+            ->button(),
+
+
+            // export 
+            // Actions\ActionGroup::make([
+
+                Actions\ExportAction::make()
+                ->label('Export')
+                ->exporter(InternationalRateExporter::class),
+
                 // Actions\ImportAction::make()
                 // ->label('Import International Text Rate')
                 // ->importer(InternationalRate::class)
@@ -36,18 +59,20 @@ class ListInternationalRates extends ListRecords
                 //     'updateExisting' => true,
                 //     'type' => InternationalRate::TYPE_SMS
                 // ])
-            ])
-            ->label('More actions')
-            ->icon('heroicon-m-ellipsis-vertical')
-            ->size(ActionSize::Small)
-            ->color('primary')
-            ->button(),
+            // ])
+            // ->label('Export')
+            // ->icon('heroicon-m-ellipsis-vertical')
+            // ->size(ActionSize::Small)
+            // ->color('primary')
+            // ->button(),
 
 
             Actions\CreateAction::make()
             ->label('Add InternationRate'),
         ];
     }
+
+
 
     public function getTabs(): array
     {
